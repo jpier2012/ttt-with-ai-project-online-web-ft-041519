e5025d60bc5409715c4a9ffad2472c7448c5b9e8
James Pier <jpier2012@gmail.com>
2019-05-16 01:48:25 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -34,7 +34,7 @@ class Game
   def turn
     puts "please enter a number"
     turn
-    
+
     move = current_player.move(@board.cells)
     if @board.valid_move?(move)
       @board.update(move.to_i - 1, current_player)
