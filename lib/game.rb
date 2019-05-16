8e0160550402cadd76ac3619f4eebd1b1fb928e7
James Pier <jpier2012@gmail.com>
2019-05-16 02:16:41 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -32,15 +32,15 @@ class Game
   end
 
   def turn
-    puts "please enter a number"
-    move = current_player.move(@board.cells)
-    binding.pry
-    if @board.valid_move?(move)
-      @board.update(move.to_i - 1, current_player)
-    else
-      puts "Please enter a valid move"
-      turn
+    move = ""
+    until @board.valid_move?(move)
+      puts "please enter a number"
+      move = current_player.move(@board.cells)
+      if @board.valid_move?(move)
+        @board.update(move.to_i - 1, current_player)
+      else
+        puts "Please enter a valid move"
+      end
     end
-    turn
   end
 end
